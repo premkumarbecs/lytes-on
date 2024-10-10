3f64231bb7a1c178c92460b10db6b5dab6a56a61
Prem <prem@zethic.com>
2024-10-10 10:21:20 +0530

    fix: change endpint name

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WebhookController;
-    
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -19,4 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/webhook', [WebhookController::class, 'handleWebhook']);
+Route::post('/receive-data', [WebhookController::class, 'handleWebhook']);
