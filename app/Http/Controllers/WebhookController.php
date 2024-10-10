d820925b735a86d3a0f520795f4940c0b5d94198
Prem <prem@zethic.com>
2024-10-10 10:13:55 +0530

    first commit

---
+++ app/Http/Controllers/WebhookController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+
+
+class WebhookController extends Controller
+{
+    public function handleWebhook(Request $request)
+    {
+        // Get all the data sent by the webhook
+        $data = $request->all();
+
+        // Log the incoming webhook data
+        Log::info('Webhook received: ', $data);
+
+        // Return a 200 OK response to acknowledge the webhook
+        return response()->json(['status' => 'success'], 200);
+    }
+}
